026574427260c34109671ddd6e846cf8e633a1ae
ahmed ismail <ahmed.ismail.fintech@gmail.com>
2026-01-07 01:01:22 +0200

    fix the locle

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,9 @@
 import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
-  // locales: ["ar", "en"],
-  // defaultLocale: "en",
-  // localePrefix: "as-needed",
   locales: ["en", "ar"],
   defaultLocale: "en",
-
   localePrefix: "always",
-
 });
 
 export const config = {
